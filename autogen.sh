e7c603844d1a3af80f2d2aaac3c9c8e3674bccfe
Marc Kleine-Budde <mkl@pengutronix.de>
2005-11-29 13:32:13 +0000

    * code restructured
    git-svn-id: https://iocaste.extern.pengutronix.de/svn/canutils/trunks/canutils-1.0-trunk@54 5fd5a299-6ef2-0310-aa18-8b01d7c39d8c

--- autogen.sh
+++ autogen.sh
@@ -1,13 +1,26 @@
 #!/bin/sh
+
+#
+# usage:
+#
+# banner <target name>
+#
+banner() {
+
+	echo
+	TG=`echo $1 | sed -e "s,/.*/,,g"`
+	LINE=`echo $TG |sed -e "s/./-/g"`
+	echo $LINE
+	echo $TG
+	echo $LINE
+	echo
+}
+
+
 ACLOCAL=${ACLOCAL:=aclocal}
 AUTOHEADER=${AUTOHEADER:=autoheader}
 AUTOMAKE=${AUTOMAKE:=automake}
 AUTOCONF=${AUTOCONF:=autoconf}
-
-echo use aclocal: $ACLOCAL
-echo use autoheader: $AUTOHEADER
-echo use automake: $AUTOMAKE
-echo use autoconf: $AUTOCONF
 
 $ACLOCAL --version | \
    awk -vPROG="aclocal" -vVERS=1.7\
@@ -17,9 +30,25 @@ $AUTOMAKE --version | \
    awk -vPROG="automake" -vVERS=1.7\
    '{if ($1 == PROG) {gsub ("-.*","",$4); if ($4 < VERS) print PROG" < version "VERS"\nThis may result in errors\n"}}'
 
-$ACLOCAL -I config/m4 && \
-libtoolize --force && \
-$AUTOHEADER && \
-$AUTOMAKE --gnu --add-missing -Wall && \
-$AUTOCONF -Wall
 
+banner "running libtoolize"
+libtoolize --force
+[ $? != 0 ] && exit 
+
+banner "running aclocal"
+$ACLOCAL -I config/m4 
+[ $? != 0 ] && exit 
+
+banner "running autoheader"
+$AUTOHEADER
+[ $? != 0 ] && exit 
+
+banner "running automake"
+$AUTOMAKE --gnu --add-missing -Wall
+[ $? != 0 ] && exit 
+
+banner "running autoconf"
+$AUTOCONF -Wall
+[ $? != 0 ] && exit 
+
+banner "Finished"
